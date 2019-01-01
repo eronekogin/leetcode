11303d6253a1b290b9085935815b15fbe4d7b777
Ian Jiang <antbear@qq.com>
2019-01-01 20:54:10 +0800

    Complete two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -2,24 +2,22 @@
 https://leetcode.com/problems/two-sum/
 """
 
-def twoSum(nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    chkDict = {}
-
-    for i in range(len(nums)):
-        num = nums[i]
-
-        chkNum = target - num
-        rsltPos = chkDict.get(chkNum)
-
-        if rsltPos is not None and rsltPos != i:
-            return [rsltPos, i]
-
-        chkDict[num] = i
-
-
-print(twoSum([2, 7, 11, 15], 9))
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        chkDict = {}
+        
+        for i in range(len(nums)):
+            num = nums[i]
+            
+            chkNum = target - num
+            rsltPos = chkDict.get(chkNum)
+            
+            if rsltPos is not None and rsltPos != i:
+                return [rsltPos, i]
+            
+            chkDict[num] = i

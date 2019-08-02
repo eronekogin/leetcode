a9efb94df0fb20226e977e5f21b3daae16b136cf
Ian Jiang <mengyu.jiang@gmail.com>
2019-08-02 13:47:08 +0800

    Complete permutations.py

---
+++ Accepted/permutations.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problems/permutations/
+"""
+
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        rslt, workRslt = [[]], []
+        for num in nums:
+            for preRslt in rslt:
+                for i in range(len(preRslt) + 1):
+                    # Insert the next char to any of the positon of
+                    # the pre premutations.
+                    workRslt.append(preRslt[:i] + [num] + preRslt[i:])
+
+            rslt = workRslt.copy()
+            workRslt.clear()
+
+        return rslt
+
+
+nums = [1, 2, 3]
+print(Solution().permute(nums))

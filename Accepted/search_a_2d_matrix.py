949bc2829ea68cca4e168ceb55df00a4891ea30c
Ian Jiang <mengyu.jiang@gmail.com>
2019-08-29 11:22:10 +0800

    Optimized search_a_2d_matrix.py

--- Accepted/search_a_2d_matrix.py
+++ Accepted/search_a_2d_matrix.py
@@ -10,35 +10,30 @@ class Solution:
             return False
 
         # Search row.
-        row = self.binary_search(
-            [matrix[i][0] for i in range(len(matrix))], target)
-        if row >= 0:
-            if matrix[row][0] == target:
+        l, r = 0, len(matrix) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[m][0]:
+                l = m + 1
+            elif target < matrix[m][0]:
+                r = m - 1
+            else:
                 return True
 
-            # Search column.
-            col = self.binary_search(matrix[row], target)
-            if col >= 0:
-                if matrix[row][col] == target:
+        # Search column.
+        if r >= 0:
+            nums = matrix[r]
+            l, r = 0, len(nums) - 1
+            while l <= r:
+                m = (l + r) // 2
+                if target > nums[m]:
+                    l = m + 1
+                elif target < nums[m]:
+                    r = m - 1
+                else:
                     return True
 
         return False
-
-    def binary_search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-        if target < nums[0]:
-            return -1
-
-        while l <= r:
-            m = (l + r) // 2
-            if target > nums[m]:
-                l = m + 1
-            elif target < nums[m]:
-                r = m - 1
-            else:
-                return m
-
-        return r  # When target > nums[-1].
 
 
 matrix = [[1, 3]]

5a29f59f5064df1ab54cb758d3be85e63cce72e4
Ian Jiang <mengyu.jiang@gmail.com>
2019-12-03 10:54:52 +0800

    Completed second_highest_salary.sql

--- Accepted/combine_two_tables.sql
+++ Accepted/combine_two_tables.sql
@@ -2,7 +2,7 @@
 
 
 -- Write your MySQL query statement below
-select p.FirstName, p.LastName, a.City, a.State
-from Person as p left outer join Address as a
-on p.PersonId = a.PersonId
+SELECT p.FirstName, p.LastName, a.City, a.State
+FROM Person as p LEFT JOIN Address AS a
+ON p.PersonId = a.PersonId
 ;

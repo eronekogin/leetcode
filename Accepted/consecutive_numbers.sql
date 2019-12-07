d8024fcca1876e8d59c8a7ec5564b1c3070c81c2
Ian Jiang <mengyu.jiang@gmail.com>
2019-12-07 12:23:27 +0800

    Completed consecutive_numbers.sql

---
+++ Accepted/consecutive_numbers.sql
@@ -0,0 +1,10 @@
+-- https://leetcode.com/articles/consecutive-numbers/
+
+-- Presumption: The id column in Logs table is consecutive.
+
+SELECT DISTINCT L1.Num AS ConsecutiveNums
+FROM Logs L1, Logs L2, Logs L3
+WHERE L2.Id = L1.Id + 1
+AND L3.Id = L2.Id + 1
+AND L1.Num = L2.Num
+AND L2.Num = L3.Num

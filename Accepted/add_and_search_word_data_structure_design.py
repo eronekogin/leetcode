d12a75fc63341026156aef98eaef5a826d886693
Ian Jiang <mengyu.jiang@gmail.com>
2020-01-11 15:40:06 +0800

    Completed add_and_search_word_data_structure_design.py

---
+++ Accepted/add_and_search_word_data_structure_design.py
@@ -0,0 +1,58 @@
+"""
+https://leetcode.com/problems/add-and-search-word-data-structure-design/
+"""
+
+
+from collections import deque
+
+
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        currNode = self.root
+        for c in word:
+            if c not in currNode:
+                currNode[c] = {}
+
+            currNode = currNode[c]
+
+        currNode[' '] = {}  # Indicate the end of the word.
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. 
+        A word could contain the dot character '.' to represent any one letter.
+        """
+        n, queue = len(word), deque([(0, self.root)])
+        while queue:
+            i, currNode = queue.popleft()
+            if i == n:
+                if ' ' in currNode:
+                    return True
+            else:
+                currChar, nextLevel = word[i], i + 1
+                if currChar == '.':
+                    for nextNode in currNode.values():
+                        queue.append((nextLevel, nextNode))
+                else:
+                    if currChar in currNode:
+                        queue.append((nextLevel, currNode[currChar]))
+
+        return False
+
+
+w = WordDictionary()
+w.addWord('bad')
+w.addWord('dad')
+w.addWord('mad')
+w.search('.ad')
+w.search('b..')

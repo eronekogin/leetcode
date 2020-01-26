d68187f6de8fbe150e2529f9f4ef7313eaa6b02e
Ian Jiang <mengyu.jiang@gmail.com>
2020-01-26 10:45:19 +0800

    Completed basic_calculator.py

---
+++ Accepted/basic_calculator.py
@@ -0,0 +1,31 @@
+"""
+https://leetcode.com/problems/basic-calculator/
+"""
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack, operand, sign, rslt = [], 0, 1, 0
+        for c in s:
+            if c.isdecimal():  # '0' ~ '9'.
+                operand = operand * 10 + int(c)
+            elif c == '+':
+                rslt += sign * operand
+                operand, sign = 0, 1
+            elif c == '-':
+                rslt += sign * operand
+                operand, sign = 0, -1
+            elif c == '(':
+                stack.append(rslt)
+                stack.append(sign)
+                rslt, sign = 0, 1
+            elif c == ')':
+                rslt += sign * operand
+                rslt *= stack.pop()  # First poped is the sign.
+                rslt += stack.pop()  # Then poped is the operand.
+                operand = 0
+
+        return rslt + sign * operand
+
+
+print(Solution().calculate("12 + 34"))

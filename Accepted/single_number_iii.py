72f52df00020a9b8fea6b06ef18b170ca2d63895
Ian Jiang <mengyu.jiang@gmail.com>
2020-02-11 13:03:42 +0800

    Completed single_number_iii.py

---
+++ Accepted/single_number_iii.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/single-number-iii/
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        ab = 0  # Stores the xor of the two unique number.
+        for num in nums:
+            ab ^= num
+
+        # Then get the rightmost bit where a and b is different.
+        diff = ab & ~(ab - 1)
+
+        # Partition the numbers into two groups which have this rightmost
+        # bit set or not.
+        a = b = 0
+        for num in nums:
+            if num & diff:
+                a ^= num
+            else:
+                b ^= num
+
+        return [a, b]

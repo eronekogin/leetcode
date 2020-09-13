44be13ade2619e3cf7127d5ed01fdfccddab6fa7
Ian Jiang <mengyu.jiang@gmail.com>
2020-09-13 20:45:30 +0800

    Completed reverse_words_in_a_string_iii.py

---
+++ Accepted/reverse_words_in_a_string_iii.py
@@ -0,0 +1,8 @@
+"""
+https://leetcode.com/problems/reverse-words-in-a-string-iii/
+"""
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(w[::-1] for w in s.split())

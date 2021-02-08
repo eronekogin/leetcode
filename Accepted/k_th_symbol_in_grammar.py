ffc16400f6d2e0aa7054ea7c63958d191311c582
Ian Jiang <mengyu.jiang@gmail.com>
2021-02-08 11:46:48 +0800

    Completed k_th_symbol_in_grammar.py

---
+++ Accepted/k_th_symbol_in_grammar.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/problems/k-th-symbol-in-grammar/
+"""
+
+
+class Solution:
+    def kthGrammar(self, N: int, K: int) -> int:
+        """
+        1. For each adjancent row, the second row's first half is the same as
+            the first row and the second row's second half is the complement of
+            the first half.
+        2. If K is within the first half of the row, we go ahead and check
+            where K is in the previous row.
+        3. If K is within the second half of the row, we go ahead and check
+            where K is in the previous row, but return its complement as the
+            result.
+        """
+        if N == 1 and K == 1:
+            return 0
+
+        m = 2 ** (N - 2)
+        if K <= m:
+            return self.kthGrammar(N - 1, K)
+        else:
+            return 1 - self.kthGrammar(N - 1, K - m)
+
+
+print(Solution().kthGrammar(4, 5))

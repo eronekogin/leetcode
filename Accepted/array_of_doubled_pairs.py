21a0f86d3ae0f776f54d60aeaa8b8c8c563696ff
eronekogin <mengyu.jiang@gmail.com>
2021-07-14 19:34:06 +0800

    feat: completed array_of_doubled_pairs.py

---
+++ Accepted/array_of_doubled_pairs.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/array-of-doubled-pairs/
+"""
+
+
+from collections import Counter
+
+
+class Solution:
+    def canReorderDoubled(self, arr: list[int]) -> bool:
+        memo = Counter(arr)
+        for x in sorted(memo, key=lambda x: abs(x)):
+            if memo[x << 1] < memo[x]:
+                return False
+            else:
+                memo[x << 1] -= memo[x]
+
+        return True
+
+
+print(Solution().canReorderDoubled([1, 2, 4, 16, 8, 4]))

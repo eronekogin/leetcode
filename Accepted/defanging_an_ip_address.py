29653e624d1f5bcb3cadb9a2ac82dcab7988261f
eronekogin <mengyu.jiang@gmail.com>
2021-10-29 20:16:36 +0800

    feat: completed defanging_an_ip_address.py

---
+++ Accepted/defanging_an_ip_address.py
@@ -0,0 +1,8 @@
+"""
+https://leetcode.com/problems/defanging-an-ip-address/
+"""
+
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return '[.]'.join(address.split('.'))

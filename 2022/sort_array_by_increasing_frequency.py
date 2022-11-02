b56dd2e1128ffd83cff9773655b8941cb203747c
eronekogin <mengyu.jiang@gmail.com>
2022-11-02 08:38:48 +0800

    feat: completed sort_array_by_increasing_frequency.py

---
+++ 2022/sort_array_by_increasing_frequency.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/sort-array-by-increasing-frequency/
+"""
+
+
+from collections import Counter
+
+
+class Solution:
+    def frequencySort(self, nums: list[int]) -> list[int]:
+        cnt = Counter(nums)
+        return sorted(nums, key=lambda x: (cnt[x], -x))

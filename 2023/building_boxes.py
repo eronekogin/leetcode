e0b6df084536dc2bf398fce0f086067f4dc8cdb8
eronekogin <mengyu.jiang@gmail.com>
2023-02-02 11:31:14 +0800

    feat: completed building_boxes.py

---
+++ 2023/building_boxes.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/building-boxes/
+"""
+
+
+class Solution:
+    def minimumBoxes(self, n: int) -> int:
+        cur = i = j = 0
+        while cur < n:
+            j += 1
+            i += j
+            cur += i
+
+        if cur == n:
+            return i
+
+        cur -= i
+        i -= j
+        j = 0
+        while cur < n:
+            j += 1
+            cur += j
+
+        return i+j

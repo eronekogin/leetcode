95cbab4ba9ab5e468180d261b4ea2c7c50db6c25
eronekogin <mengyu.jiang@gmail.com>
2023-03-21 23:58:17 +0800

    feat: completed make_the_xor_of_all_segments_equal_to_zero.py

---
+++ 2023/make_the_xor_of_all_segments_equal_to_zero.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/make-the-xor-of-all-segments-equal-to-zero/
+"""
+
+
+class Solution:
+    def minChanges(self, nums: list[int], k: int) -> int:
+        LIMIT = 2**10
+        mrr = [[0 for _ in range(LIMIT)] for _ in range(k)]
+        for i, x in enumerate(nums):
+            mrr[i % k][x] += 1
+
+        dp = [-2000 for _ in range(LIMIT)]
+        dp[0] = 0
+        for row in mrr:
+            maxprev = max(dp)
+            new_dp = [maxprev for _ in range(LIMIT)]
+            for i, cnt in enumerate(row):
+                if cnt > 0:
+                    for j, prev in enumerate(dp):
+                        new_dp[i ^ j] = max(new_dp[i ^ j], prev+cnt)
+            dp = new_dp
+
+        return len(nums) - new_dp[0]

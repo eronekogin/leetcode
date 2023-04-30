f6b1ebb207e0bb093d393e24779ce35ef91d6e7a
eronekogin <mengyu.jiang@gmail.com>
2023-04-30 11:02:22 +0800

    feat: completed sign_of_the_product_of_an_array.py

---
+++ 2023/sign_of_the_product_of_an_array.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/sign-of-the-product-of-an-array/
+"""
+
+
+class Solution:
+    def arraySign(self, nums: list[int]) -> int:
+        sign = 1
+        for num in nums:
+            if num == 0:
+                return 0
+
+            if num < 0:
+                sign = -sign
+
+        return sign

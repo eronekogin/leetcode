1ef3aee2ed92e5c8869a9dadd8f0b85a6dc76dd1
eronekogin <mengyu.jiang@gmail.com>
2023-05-08 08:30:35 +0800

    feat: completed check_if_the_sentence_is_pangram.py

---
+++ 2023/check_if_the_sentence_is_pangram.py
@@ -0,0 +1,8 @@
+"""
+https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+"""
+
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        return len(set(sentence)) == 26

7a452f422a00efe864a64d2b6a5b3beb00a3884e
eronekogin <mengyu.jiang@gmail.com>
2023-06-20 09:03:20 +0800

    feat: completed substrings_of_size_three_with_distinct_characters.py

---
+++ 2023/substrings_of_size_three_with_distinct_characters.py
@@ -0,0 +1,9 @@
+"""
+https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
+"""
+
+
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        return sum(len(set(s[i: i + 3])) == 3 for i in range(len(s) - 2))
+                

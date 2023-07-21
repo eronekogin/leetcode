1a38a7546f315dee1ec654fab2a94d5c3c5c8787
eronekogin <mengyu.jiang@gmail.com>
2023-07-21 08:13:44 +0800

    feat: completed count_salary_categories.sql

---
+++ 2023/count_salary_categories.sql
@@ -0,0 +1,20 @@
+--- https://leetcode.com/problems/count-salary-categories/
+
+
+
+select 'Low Salary' as category, count(*) as accounts_count
+from accounts
+where income < 20000
+
+union
+
+select 'Average Salary' as category, count(*) as accounts_count
+from accounts
+where income between 20000 and 50000
+
+union
+
+select 'High Salary' as category, count(*) as accounts_count
+from accounts
+where income > 50000
+

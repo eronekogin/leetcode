36d5aff6adbea7a39763305dfc7f5737a4ce6825
eronekogin <mengyu.jiang@gmail.com>
2024-05-09 09:48:25 +0800

    feat: completed add_two_integers.py

---
+++ 2024/add_two_integers.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/add-two-integers/description/
+"""
+
+
+class Solution:
+    """
+    Solution
+    """
+
+    def sum(self, num1: int, num2: int) -> int:
+        """
+        sum
+        """
+        return num1 + num2

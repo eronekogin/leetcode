2c7b8ba1c9a535e41877e1ba0b34a98670b8e4c1
eronekogin <mengyu.jiang@gmail.com>
2024-08-21 12:40:27 +0800

    feat: completed number_of_unique_subjects_taught_by_each_teacher.sql

---
+++ 2024/number_of_unique_subjects_taught_by_each_teacher.sql
@@ -0,0 +1,12 @@
+-- https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/
+
+
+select
+    teacher_id, count(distinct subject_id) as cnt
+from
+    teacher
+group by
+    teacher_id
+
+
+

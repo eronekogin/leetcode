e7533576e989d875020da42d3ec2e4934fe6f585
eronekogin <mengyu.jiang@gmail.com>
2025-12-02 10:09:53 +0800

    feat: completed maximum_value_of_an_ordered_triplet_i.py

---
+++ 2025/maximum_value_of_an_ordered_triplet_i.py
@@ -0,0 +1,26 @@
+"""
+https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/description/
+"""
+
+
+class Solution:
+    """
+    Solution
+    """
+
+    def maximum_triplet_value(self, nums: list[int]) -> int:
+        """
+        maximum triplet value
+        """
+        rslt = 0
+        max_first = 0
+        max_diff = 0
+        for v in nums:
+            rslt = max(rslt, max_diff * v)
+            max_diff = max(max_diff, max_first - v)
+            max_first = max(max_first, v)
+
+        return rslt
+
+
+print(Solution().maximum_triplet_value([12, 6, 1, 2, 7]))

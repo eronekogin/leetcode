399da976fd4d7afd7bf3a0fc27c398b1217a6188
eronekogin <mengyu.jiang@gmail.com>
2026-08-08 21:17:27 +0800

    feat: completed find_the_maximum_length_of_a_good_subsequence_i.py

---
+++ 2026/find_the_maximum_length_of_a_good_subsequence_i.py
@@ -0,0 +1,39 @@
+"""
+https://leetcode.com/problems/find-the-maximum-length-of-a-good-subsequence-i/description/
+"""
+
+
+from collections import defaultdict
+
+
+class Solution:
+    """
+    Solution
+    """
+
+    def maximum_length(self, nums: list[int], k: int) -> int:
+        """
+        good_sequences[i] stands for the maximum length of
+        a good sequence with at most i different neighbour.
+
+        dp[i][x] stands for the maximum length of a good
+        sequence with at most i different neighbour and ends
+        with number x.
+
+        So dp[i][a] = max(dp[i][a] + 1, good_sequences[i - 1] + 1)
+        """
+        good_sequences = [0] * (k + 1)
+        dp = [defaultdict(int) for _ in range(k + 1)]
+
+        for x in nums:
+            for i in range(k, -1, -1):
+                dp[i][x] = max(
+                    dp[i][x] + 1,
+                    good_sequences[i - 1] + 1 if i > 0 else 0
+                )
+                good_sequences[i] = max(
+                    good_sequences[i],
+                    dp[i][x]
+                )
+
+        return good_sequences[-1]
